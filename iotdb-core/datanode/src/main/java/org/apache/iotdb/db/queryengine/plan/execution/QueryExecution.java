df00f8b22c8f5d5d9f4bb600e897841c391e7041
spring <24B903032@stu.hit.edu.cn>
2026-03-16 23:07:08 +0800

    修改analyze语句bug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/execution/QueryExecution.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/execution/QueryExecution.java
@@ -19,9 +19,11 @@
 package org.apache.iotdb.db.queryengine.plan.execution;
 
 import org.apache.iotdb.common.rpc.thrift.TEndPoint;
+import org.apache.iotdb.common.rpc.thrift.TRegionReplicaSet;
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.commons.conf.IoTDBConstant;
 import org.apache.iotdb.commons.exception.IoTDBException;
+import org.apache.iotdb.commons.partition.ExecutorType;
 import org.apache.iotdb.commons.service.metric.PerformanceOverviewMetrics;
 import org.apache.iotdb.db.conf.IoTDBConfig;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
@@ -339,19 +341,35 @@ public class QueryExecution implements IQueryExecution {
         }
 
 //        System.out.println("\n-----------\ngetType:"+distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet().getRegionId().getType());
-        if(distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet()!=null){
-            if(distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet()==null ||
-                    distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet().getRegionId().getType()==DataRegion) {
-                String edgeQueryId = context.getQueryId().getId();
-                int dataNodeId = IoTDBDescriptor.getInstance().getConfig().getDataNodeId();
-                String colQueryId = edgeQueryId + "-" + dataNodeId;
-                QueryStateManager queryStateManager = ColQuerySessions.getByEdgeQueryId(colQueryId);
-                //设置根节点
-                queryStateManager.setRootIdentitySinkId(distributedPlan.getInstances().get(0).getFragment().getPlanNodeTree().getPlanNodeId());
-//            System.out.println("\nFragmentInstances:"+printFragmentInstances(distributedPlan.getInstances()));
-//            System.out.println("\nRoot Identity's PlanNodeId is:"+distributedPlan.getInstances().get(0).getFragment().getPlanNodeTree().getPlanNodeId().getId());
+        List<FragmentInstance> instances = distributedPlan.getInstances();
+        if (instances != null && !instances.isEmpty()) {
+            FragmentInstance instance = instances.get(0);
+            ExecutorType executorType = instance.getExecutorType();
+            if (executorType != null) {
+                TRegionReplicaSet regionReplicaSet = executorType.getRegionReplicaSet();
+                // 根据业务逻辑判断：如果regionReplicaSet为null或者其region类型是DataRegion
+                if (regionReplicaSet == null || regionReplicaSet.getRegionId().getType() == DataRegion) {
+                    String edgeQueryId = context.getQueryId().getId();
+                    int dataNodeId = IoTDBDescriptor.getInstance().getConfig().getDataNodeId();
+                    String colQueryId = edgeQueryId + "-" + dataNodeId;
+                    QueryStateManager queryStateManager = ColQuerySessions.getByEdgeQueryId(colQueryId);
+                    queryStateManager.setRootIdentitySinkId(instance.getFragment().getPlanNodeTree().getPlanNodeId());
+                }
             }
         }
+//        if(distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet()!=null){
+//            if(distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet()==null ||
+//                    distributedPlan.getInstances().get(0).getExecutorType().getRegionReplicaSet().getRegionId().getType()==DataRegion) {
+//                String edgeQueryId = context.getQueryId().getId();
+//                int dataNodeId = IoTDBDescriptor.getInstance().getConfig().getDataNodeId();
+//                String colQueryId = edgeQueryId + "-" + dataNodeId;
+//                QueryStateManager queryStateManager = ColQuerySessions.getByEdgeQueryId(colQueryId);
+//                //设置根节点
+//                queryStateManager.setRootIdentitySinkId(distributedPlan.getInstances().get(0).getFragment().getPlanNodeTree().getPlanNodeId());
+//            System.out.println("\nFragmentInstances:"+printFragmentInstances(distributedPlan.getInstances()));
+//            System.out.println("\nRoot Identity's PlanNodeId is:"+distributedPlan.getInstances().get(0).getFragment().getPlanNodeTree().getPlanNodeId().getId());
+//            }
+//        }
 //    distributedPlan.getInstances().get(0)
 
 
